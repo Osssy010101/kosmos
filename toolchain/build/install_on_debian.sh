c4971a366f2b6c74ce1fcc2f1cd6fdcc2bec3e73
Ossys010101 <ossys10101101@gmail.com>
2025-11-17 00:31:30 +0200

    Create script that builds OS (and its compiler) from scratch on debian distro

---
+++ toolchain/build/install_on_debian.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rep_name='Operation-System'
+
+apt install make nasm gcc xorriso qemu-system texinfo build-essential bison flex git-lfs -y
+
+cd $rep_name
+
+git lfs install
+git lfs pull
+
+sudo ./install.sh
+
+export PATH=$PATH:/opt/i386-elf
+export PATH=$PATH:/opt/i386-elf/i386-elf/bin
+# Перезапуск сесси
+exec bash
+
+cd ../../
+
+make
