7ba649bc30be399d4d1bf7ef18a09d11624df657
igmunv <devskynger@gmail.com>
2025-11-17 00:02:53 -0500

    docs: добавлена документация по созданию драйверов

--- src/drivers/driver_list.c
+++ src/drivers/driver_list.c
@@ -13,6 +13,7 @@
     X(keyboard_driver,   "keyboard.c", VIRT_KEYBOARD_CONTROLLER, VIRT_KEYBOARD_LDEV, keyboard_init, keyboard_funcs) \
     X(display_driver,   "display.c", VIRT_DISPLAY_CONTROLLER, VIRT_DISPLAY_VGATEXT, display_init, display_funcs)
 
+// - - TemplateM - -
 #define X(name, filename, classcode, subclass, init, funcs) { #name, filename, classcode, subclass, init, funcs },
 
 struct driver_info drivers[] = {
