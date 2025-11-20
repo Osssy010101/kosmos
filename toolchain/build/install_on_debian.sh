54ff162449a1bf14304cfdab530f6669fe9f03b2
Ossys010101 <ossys10101101@gmail.com>
2025-11-21 00:29:35 +0200

    add grub-utils as a dependecy in install_on_debian.sh. It needs for running kosmos

--- toolchain/build/install_on_debian.sh
+++ toolchain/build/install_on_debian.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 apt update -y > /dev/null
-apt install make nasm gcc xorriso qemu-system texinfo build-essential bison flex git-lfs sudo xz-utils -y > /dev/null
+apt install make nasm gcc xorriso qemu-system texinfo build-essential bison flex git-lfs sudo xz-utils grub-utils -y > /dev/null
 
 git lfs install
 git lfs pull
 
-echo 'export PATH=/opt/i386-elf:$PATH' >> ~/.bashrc
+echo 'export PATH=/opt/i386-elf/bin:$PATH' >> ~/.bashrc
 
 exec sh
