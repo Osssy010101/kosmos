5b027b747367256fe1a88d10f9eb25722948bcb4
Ossys010101 <ossys10101101@gmail.com>
2025-11-20 23:10:24 +0200

    now compiler just unpack from tar (becauze when i compile it by myself from scrath there is not i386-elf-gcc for some reason)

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,10 @@ COPY ./toolchain /kosmos_os/toolchain
 COPY ./iso /kosmos_os/iso
 COPY ./Makefile /kosmos_os/Makefile
 
-WORKDIR /kosmos_os/toolchain/build/
+WORKDIR /kosmos_os/toolchain/build
 RUN chmod +x install_on_debian.sh
 RUN ./install_on_debian.sh
+
+WORKDIR /kosmos_os/toolchain/dists
+RUN mkdir -p /opt/i386-elf
+RUN tar xJf ./i386-elf-7.5.0-Linux-x86_64.tar.xz --strip-components=1 -C /opt/i386-elf
--- toolchain/build/install_on_debian.sh
+++ toolchain/build/install_on_debian.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-apt install make nasm gcc xorriso qemu-system texinfo build-essential bison flex git-lfs sudo -y > /dev/null
+apt update -y > /dev/null
+apt install make nasm gcc xorriso qemu-system texinfo build-essential bison flex git-lfs sudo xz-utils -y > /dev/null
 
 git lfs install
 git lfs pull
 
-if [ "$(id -u)" -ne 0 ]; then
-    sh ./install.sh
-else
-    sudo sh ./install.sh
-fi
+echo 'export PATH=/opt/i386-elf:$PATH' >> ~/.bashrc
 
-export PATH=$PATH:/opt/i386-elf
-export PATH=$PATH:/opt/i386-elf/i386-elf/bin
+exec sh
